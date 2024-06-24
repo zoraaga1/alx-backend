6aed676ed56108c735ff0fc08f5268b4eb5c9e98
zoraaga1 <fati.sousse12@gmail.com>
2024-06-24 23:00:06 +0100

    task0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""0-simple_helper_function module"""
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Return a tuple of size two containing a start index and an end index"""
+    return ((page - 1) * page_size, page * page_size)
